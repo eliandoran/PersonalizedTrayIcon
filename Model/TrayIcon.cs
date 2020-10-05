b2c6026bf21f89fc4f1e1952f58dc53693b9ac1e
Elian Doran <doran.adoris.elian@gmail.com>
2020-10-05 18:22:34 +0300

    Create model for configuration

---
+++ Model/TrayIcon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalizedTrayIcon.Model
+{
+    class TrayIcon
+    {
+
+        public string IconPath { get; set; }
+
+        public string ExecPath { get; set; }
+
+    }
+}
---
+++ Model/UserConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalizedTrayIcon.Model
+{
+    class UserConfiguration
+    {
+
+        public List<TrayIcon> Icons;
+
+    }
+}
