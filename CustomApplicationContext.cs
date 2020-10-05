3f786696513f698095d75bb555511e0a7b6aeaab
Elian Doran <doran.adoris.elian@gmail.com>
2020-10-05 18:22:25 +0300

    Create custom application context

---
+++ CustomApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PersonalizedTrayIcon
+{
+    class CustomApplicationContext: ApplicationContext
+    {
+
+        public CustomApplicationContext()
+        {
+
+        }
+
+    }
+}
